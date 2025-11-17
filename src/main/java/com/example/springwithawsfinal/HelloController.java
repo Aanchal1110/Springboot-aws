29e1cf6ab50e432b99cd9a66c6313f02b1f74101
Aanchal1110 <aanchalupadhyay1009@gmail.com>
2025-11-17 10:26:30 +0530

    Created a post request

--- src/main/java/com/example/springwithawsfinal/HelloController.java
+++ src/main/java/com/example/springwithawsfinal/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.springwithawsfinal;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello there mate!";
+    }
+
+    @PostMapping("/hello")
+    public String hell(@RequestBody String name){
+        return "Hello" + name+"!";
     }
 }
