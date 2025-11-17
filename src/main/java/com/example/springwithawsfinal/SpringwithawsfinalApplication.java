45803f848dd4a370d488dafa788f0914df214a06
Aanchal1110 <aanchalupadhyay1009@gmail.com>
2025-11-17 09:16:51 +0530

    Creating a simple restapi

---
+++ src/main/java/com/example/springwithawsfinal/SpringwithawsfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwithawsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithawsfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithawsfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwithawsfinal/SpringwithawsfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwithawsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithawsfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
